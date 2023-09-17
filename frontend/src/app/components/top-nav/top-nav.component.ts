1a07af4974783d7af5661967816eb9e9d555a037
Go-Adler <gokul_adler@outlook.com>
2023-09-17 23:13:52 +0530

    fixed sticky

--- frontend/src/app/components/top-nav/top-nav.component.ts
+++ frontend/src/app/components/top-nav/top-nav.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service'
 
 @Component({
   selector: 'app-top-nav',
   templateUrl: './top-nav.component.html',
   styleUrls: ['./top-nav.component.scss']
 })
-export class TopNavComponent {
+export class TopNavComponent implements OnInit{
+  profilePicture:string = ''
 
+  constructor(
+    private userService: UserService
+  ) {}
+  
+  ngOnInit(): void {
+    this.userService.profilePicture$.subscribe( profilePicture => {
+      this.profilePicture = profilePicture
+    })
+  }
 }
