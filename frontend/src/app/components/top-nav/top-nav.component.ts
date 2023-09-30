a1ec7c914cca46e39b3ccfabbbf67fa6dddbb475
Go-Adler <gokul_adler@outlook.com>
2023-09-30 22:42:30 +0530

    fixed logout responsively

--- frontend/src/app/components/top-nav/top-nav.component.ts
+++ frontend/src/app/components/top-nav/top-nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button'
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog'
 import { UserService } from 'src/app/services/user.service'
+import { LogOutService } from '../log-in/log-out.service'
 
 @Component({
   selector: 'app-top-nav',
@@ -8,14 +11,50 @@ import { UserService } from 'src/app/services/user.service'
 })
 export class TopNavComponent implements OnInit{
   profilePicture: string = ''
+  userName: string = '';
+  logOutPill: boolean = false;
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    public dialog: MatDialog
   ) {}
   
   ngOnInit(): void {
     this.userService.profilePicture$.subscribe( profilePicture => {
       this.profilePicture = profilePicture
     })
+
+     // Get user name
+     this.userService.userName$.subscribe((userName) => {
+      this.userName = userName;
+    });
+  }
+
+  showLogOut() {
+    this.logOutPill = !this.logOutPill
+  }
+
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(DialogAnimationsExampleDialog, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
   }
 }
+
+@Component({
+  selector: 'dialog-animations-example-dialog',
+  templateUrl: 'dialog-animations-example-dialog.html',
+  styleUrls: ['./side-nav.component.scss'],
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+})
+export class DialogAnimationsExampleDialog {
+  constructor(public dialogRef: MatDialogRef<DialogAnimationsExampleDialog>, public logOutService: LogOutService) { }
+
+  logOut() {
+    this.logOutService.logOut()
+  }
+}
+
