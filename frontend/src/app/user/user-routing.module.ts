bdb76d327d83fe03e9d7057fbbe0819a3d113c72
Go-Adler <gokul_adler@outlook.com>
2023-10-14 21:27:01 +0530

    fixing routing

--- frontend/src/app/user/user-routing.module.ts
+++ frontend/src/app/user/user-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component'
+import { PostLoginComponent } from './post-login/post-login.component'
 
 const routes: Routes = [
   {
     path: 'user',
     pathMatch: 'full',
     redirectTo: 'user/auth/log-in'
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'user/auth/log-in'
+  },
+  {
+    path: '',
+    component: PostLoginComponent,
+    loadChildren: () => import('./post-login/post-login.module').then(m => m.PostLoginModule)
   },
   {
     path: 'auth',
