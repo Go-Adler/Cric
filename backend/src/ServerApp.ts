72834e7e279020de24236037e6d465d5e15173f8
Go-Adler <gokul_adler@outlook.com>
2023-10-26 12:48:23 +0530

    fixing socket.io

--- backend/src/ServerApp.ts
+++ backend/src/ServerApp.ts
@@ -1,7 +1,6 @@
 import express, { Application, Request, Response } from "express"
 import morgan from "morgan"
 import cors from "cors"
-import WebSocket from "ws"
 import http from "http"
 
 import { userRoutes } from "./modules/user/interface/routes/user.routes"
@@ -11,18 +10,15 @@ import { mongo } from "./config/database"
 export class ServerApp {
   private app: Application
   private server: http.Server
-  private wss: WebSocket.Server
   private port!: string
 
   constructor(app: Application) {
     this.port = process.env.PORT!
     this.app = app
     this.server = http.createServer(this.app)
-    this.wss = new WebSocket.Server({ server: this.server })
     this.initializeMiddlewares()
     this.initializeRoutes()
     this.initializeErrorHandling()
-    this.initializeWebSocket()
     mongo()
   }
 
@@ -46,20 +42,6 @@ export class ServerApp {
     this.app.use((err: Error, _req: Request, res: Response) => {
       console.error("Error:", err.message)
       res.status(500).json({ error: "Internal Server Error" })
-    })
-  }
-
-  private initializeWebSocket() {
-    console.log(53)
-
-    this.wss.on("connection", (ws) => {
-      console.log("initialised connections")
-      ws.on("message", (message) => {
-        console.log("Received:", message)
-        ws.send('this is really nice')
-      })
-      ws.send("Hello from server!")
-
     })
   }
 
--- frontend/src/app/user/post-login/home/home.component.ts
+++ frontend/src/app/user/post-login/home/home.component.ts
@@ -1,29 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { WebSocketService } from './websocket.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   newPostData: any;
-
-  constructor(private wsService: WebSocketService) { }
-  
-  ngOnInit(): void {
-    this.wsService.onMessage().subscribe((message) => {
-      console.log('Received:', message);
-    });
-  }
 
   newPostSuccess(postData: any) {
     this.newPostData = postData;
-  }
-
-  callSocket() {
-    console.log(16);
-    
-    this.wsService.sendMessage('s')
   }
 }
