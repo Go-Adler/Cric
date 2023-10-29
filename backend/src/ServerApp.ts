d7927597807467c6f32dea91b1279426ec7077dd
Go-Adler <gokul_adler@outlook.com>
2023-10-29 11:33:42 +0530

    fixing notifications

--- backend/src/ServerApp.ts
+++ backend/src/ServerApp.ts
@@ -2,6 +2,8 @@ import express, { Application, Request, Response } from "express"
 import morgan from "morgan"
 import cors from "cors"
 import http from "http"
+import { Server } from 'socket.io'
+import rateLimit from "express-rate-limit";
 
 import { userRoutes } from "./modules/user/interface/routes/user.routes"
 import { adminRoutes } from "./modules/admin/interface/routes/admin.routes"
@@ -10,22 +12,35 @@ import { mongo } from "./config/database"
 export class ServerApp {
   private app: Application
   private server: http.Server
+  private io: Server
   private port!: string
 
   constructor(app: Application) {
     this.port = process.env.PORT!
     this.app = app
     this.server = http.createServer(this.app)
+    this.io = new Server(this.server, {
+      cors: {
+        origin: "http://localhost:4200",
+        methods: ["GET", "POST"],
+      },
+    })
     this.initializeMiddlewares()
     this.initializeRoutes()
     this.initializeErrorHandling()
+    this.socketIoSetup()
     mongo()
   }
 
   private initializeMiddlewares() {
+    const limiter = rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 100, 
+    });
     this.app.use(cors())
     this.app.use(morgan("dev"))
     this.app.use(express.json())
+    this.app.use(limiter)
   }
 
   private initializeRoutes() {
@@ -42,6 +57,18 @@ export class ServerApp {
     this.app.use((err: Error, _req: Request, res: Response) => {
       console.error("Error:", err.message)
       res.status(500).json({ error: "Internal Server Error" })
+    })
+  }
+
+  private socketIoSetup() {
+    console.log(53);
+    this.io.on("connection", socket => {
+      console.log(`Socket connected: ${socket.id}`, 53);
+      socket.emit('message', 'hello fron the server')
+      socket.on('client-event', data => {
+        console.log('Message recieved from client', data, 56);
+      })
+      
     })
   }
 
--- frontend/src/app/user/post-login/post-login.component.ts
+++ frontend/src/app/user/post-login/post-login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { io } from 'socket.io-client'
+import { ConfigService } from 'src/app/services/config.service'
 
 @Component({
   selector: 'app-post-login',
@@ -6,4 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-login.component.scss']
 })
 export class PostLoginComponent {
+  private socket: any
+
+  constructor(private config: ConfigService) {
+    const API_URL = config.getAPI_BaseURL()
+    console.log(API_URL, 15);
+    console.log(10);
+    this.socket = io(API_URL)
+    
+  }
 }
