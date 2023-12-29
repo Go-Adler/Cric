b6df74ad9630aa46675f0ae540e206736d9bcbe9
Go-Adler <gokul_adler@outlook.com>
2023-12-29 15:44:39 +0530

    fixed env

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,18 @@
+const BUCKET_BASE_URL = 'https://cric-public-v2.s3.ap-south-1.amazonaws.com/assets/'
+
+export const environment = {
+  CRIC_LOGO: `${BUCKET_BASE_URL}cricLogo.png`,  
+  EMPTY_POST: `${BUCKET_BASE_URL}noPosts.png`, 
+  SUCCESS_ICON: `${BUCKET_BASE_URL}success.png`, 
+  LOG_OUT_IMAGE: `${BUCKET_BASE_URL}logOut.png`, 
+  MESSAGES_ICON: `${BUCKET_BASE_URL}messages.png`, 
+  POSTS_PROTECTED: `${BUCKET_BASE_URL}protected.png`, 
+  ERROR_LOADING: `${BUCKET_BASE_URL}errorLoading.png`, 
+  NO_MESSAGES_ICON: `${BUCKET_BASE_URL}noMessages.png`, 
+  FIND_FRIENDS_ICON: `${BUCKET_BASE_URL}findFriends.png`, 
+  NO_CHAT_MESSAGE_ICON: `${BUCKET_BASE_URL}noMessages.png`,
+  POST_LOADING_IMAGE: `${BUCKET_BASE_URL}loadingImage.png`, 
+  NOTIFICATIONS_SPACE: `${BUCKET_BASE_URL}notifications.png`, 
+  NO_NOTIFICATION_ICON: `${BUCKET_BASE_URL}noNotifications.png`, 
+  DEFAULT_PROFILE_PICTURE: `${BUCKET_BASE_URL}defaultProfilePictureBlack.png`, 
+}
